ccc9392e23b4443c88f93abcbcc3866efb8b9204
xkqd <danielliubchenko@gmail.com>
2025-03-22 15:57:09 +0100

    test

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import discord
 import os
+import sys
+import subprocess
+import discord
+import threading
 
 from database import Database
 from discord.ext import commands
 from dotenv import load_dotenv
+from flask import Flask, request
+
+app = Flask(__name__)
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+def run_flask():
+    app.run(host="0.0.0.0", port=5000)
+
+@app.route('/git-update', methods=['POST'])
+def git_update():
+    data = request.json
+    if "ref" in data and data["ref"] == "refs/heads/main":  # Проверяем, что обновилась ветка main
+        subprocess.run(["git", "pull", "origin", "main"])
+        os.execv(sys.executable, [sys.executable] + sys.argv)
+        return "Bot updated and restarted", 200
+    return "Ignored", 200
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -50,5 +70,7 @@ async def on_error(interaction: discord.Interaction, error):
         f"Произошла ошибка: {str(error)}.",
         ephemeral=True
     )
+
+threading.Thread(target=run_flask, daemon=True).start()
 
 bot.run(TOKEN)
